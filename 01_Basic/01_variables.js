2455a4abd0a985556bb8d43a065392e3dc1680a1
clay108 <131706067+clay108@users.noreply.github.com>
2024-03-19 20:08:52 +0000

    datatype

---
+++ 01_Basic/01_variables.js
@@ -0,0 +1,12 @@
+const accountId=12354
+let accountEmail = "hsgbxr@gmail.com"
+var accountPassword = "Patna"
+accountCity = "buxar"
+
+console.log(accountId)
+
+/*
+prefer not to use var due to scope problem
+*/
+
+console.table([accountId,accountEmail,accountPassword,accountCity])
---
+++ 01_Basic/02_dataTypes.js
@@ -0,0 +1,13 @@
+let name = "Himanshu"
+let age = 20
+
+let isAdult = true
+/*
+    number==>2 to power 53
+    bigint
+    string
+    boolean
+    null ==> standalone value
+    undefined
+    symbol ==>unique
+*/
