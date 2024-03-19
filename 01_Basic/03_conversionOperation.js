2e6f56051529e02a3281a4d6dc245ae69b001c8a
clay108 <131706067+clay108@users.noreply.github.com>
2024-03-19 20:37:14 +0000

    datatype conversion

---
+++ 01_Basic/03_conversionOperation.js
@@ -0,0 +1,10 @@
+let isLoggedIn=1
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(typeof booleanIsLoggedIn)
+console.log(booleanIsLoggedIn)
+
+let score  = 35 
+let stringNumber =  String(score)
+
+console.log(typeof stringNumber)
+console.log(stringNumber)
