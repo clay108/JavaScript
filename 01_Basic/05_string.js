2673707b843ddf97e63de3c287be72a7cb4461ed
clay108 <131706067+clay108@users.noreply.github.com>
2024-03-20 11:31:21 +0000

    string

---
+++ 01_Basic/05_string.js
@@ -0,0 +1,14 @@
+const name = "Himanshu"
+const repoCount = 50
+
+///console.log(`Hello my name is ${name} and my repoCount is ${repoCount}`)
+
+const gameName = new String ('Himanshu')
+//console.log(typeof gameName)
+
+const anotherString = "    clay   "
+console.log(anotherString)
+console.log(anotherString.trim())
+
+const url = "https://himanshu.com//%20//singh"
+console.log(url.replace('%20','&'))
