b61d9628e3cd72859ce4c3f079b4f6bacde64dc1
clay108 <131706067+clay108@users.noreply.github.com>
2024-03-20 07:45:47 +0000

    comaprison

---
+++ 01_Basic/04_comparison.js
@@ -0,0 +1,11 @@
+console.log("02">1)
+
+console.log(null > 0)
+console.log(null >= 0)
+console.log(null == 0)
+
+/*
+equality check and comparison works differently
+
+comparison converts null to a number and treated as 0
+*/
