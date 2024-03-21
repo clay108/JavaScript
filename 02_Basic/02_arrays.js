298bd126d6b5d8c0282c30c89272c9ccafc37a4a
clay108 <131706067+clay108@users.noreply.github.com>
2024-03-21 16:42:58 +0000

    array

---
+++ 02_Basic/02_arrays.js
@@ -0,0 +1,36 @@
+const marvel_heros = ["Ironman" ,"Spiderman","Thor"]
+
+const dc_heros = ["batman","superman","flash"]
+
+//marvel_heros.push(dc_heros)
+//console.log(marvel_heros)
+
+const all_heros = marvel_heros.concat(dc_heros)
+//console.log(all_heros)
+
+const all_new_heros = [...marvel_heros,...dc_heros]
+//console.log(all_new_heros)
+
+const another_array = [1,2,3,[4,5,6],[7,8,[9,10]]]
+const real_another_array = another_array.flat(Infinity)
+//console.log(real_another_array)
+
+
+console.log(Array.isArray("Hitesh"))
+console.log(Array.from("Hitesh"))
+console.log(Array.from({name: "hitesh"})) // interesting
+
+let score1 = 100
+let score2 = 200
+let score3 = 300
+
+//console.log(Array.of(score1, score2, score3));
+
+// let x=2,y=2,z=2
+
+// if(x==y==z){
+//     console.log("Enter")
+// }
+// else{
+//     console.log("Exit")
+// }
