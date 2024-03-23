83e97e5dd8535a071778bd308cdd6bb9674d94c4
clay108 <131706067+clay108@users.noreply.github.com>
2024-03-23 17:22:13 +0000

    switchCase

---
+++ 04_control_flow/switch.js
@@ -0,0 +1,24 @@
+const month = 3
+
+
+switch (month) {
+    case 1:
+        console.log("January")
+        break;
+        
+    case 2:
+        console.log("February")
+        break;
+        
+    case 3:
+        console.log("March")
+        break;
+        
+    case 4:
+        console.log("April")
+        break;
+        
+    default:
+        console.assert.log("Not matched")
+        break;
+    }
