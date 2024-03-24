a42dc35d87bfe22ecb04c7594fa3fb345bfbd9b0
clay108 <131706067+clay108@users.noreply.github.com>
2024-03-24 20:07:47 +0000

    loop

---
+++ 05_iteration/one.js
@@ -0,0 +1,11 @@
+for(let i =0 ;i<10;i++){
+    console.log(i)
+}
+
+let myArray = ["flash", "batman", "superman"]
+//console.log(myArray.length);
+for (let index = 0; index < myArray.length; index++) {
+    const element = myArray[index];
+    //console.log(element);
+    
+}
